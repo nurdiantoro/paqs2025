3a62fab532d558be52f0a4abb36231ebc300870c
Nur Diantoro <nurdiantoro100@gmail.com>
2025-03-12 22:53:22 +0700

    1.54 Revisi tabel di programe

--- resources/views/frontend/components/event_schedule.blade.php
+++ resources/views/frontend/components/event_schedule.blade.php
@@ -26,26 +26,18 @@
 <div class="flex flex-col flex-wrap justify-center items-start w-full md:w-2/3 lg:w-1/2 mx-auto">
     @foreach ($groupedProgrammes as $date => $items)
         <div
-            class="flex flex-row justify-center items-start p-4 relative before:contents[''] before:w-1 before:h-full before:bg-warna-01 before:opacity-20 before:absolute before:top-0 before:left-1/2 before:-translate-x-1/2 hover:before:opacity-100 cursor-pointer group w-full my-auto">
+            class="flex flex-col md:flex-row justify-center items-start py-2 md:p-4 relative before:contents[''] before:w-1 before:h-full before:bg-warna-01 before:opacity-20 before:absolute before:top-0 before:left-0 md:before:left-1/2 before:-translate-x-1/2 hover:before:opacity-100 cursor-pointer group w-full ">
             <div
-                class="p-4 group-hover:-translate-x-4 duration-300 group-hover:text-warna-01 w-full md:w-1/2 text-right my-auto">
+                class="pl-4 md:p-4 md:group-hover:-translate-x-4 duration-300 group-hover:text-warna-01 w-full md:w-1/2 md:text-right my-auto">
                 @php
                     // Ubah format tanggal dengan nama hari dalam bahasa Indonesia
                     \Carbon\Carbon::setLocale('en');
                     $formattedDate = Carbon::parse($date)->translatedFormat('F d, Y');
                     $formattedDay = Carbon::parse($date)->translatedFormat('l');
                 @endphp
-                <span class="block">{{ $formattedDate }}</span>
-                <span class="block">{{ $formattedDay }}</span>
+                <span class="block font-bold md:font-normal">{{ $formattedDate }}</span>
+                <span class="block font-bold md:font-normal">{{ $formattedDay }}</span>
             </div>
-            {{-- <div
-                class="ml-4 bg-neutral-50 rounded px-4 py-3 shadow-lg group-hover:translate-x-4duration-300 transition-transform group-hover:bg-warna-01 group-hover:text-neutral-50 w-full md:w-1/2 my-auto relative z-20 before:contents-[''] before:w-2 before:h-[90%] before:bg-warna-01 before:rounded-full before:absolute before:top-0 before:right-0 before:translate-y-[5%] before:translate-x-1/2">
-                <ul>
-                    @foreach ($items as $programme)
-                        <li class="block md:text-nowrap">• {{ $programme->name }}</li>
-                    @endforeach
-                </ul>
-            </div> --}}
 
             @php
                 $warna = '';
@@ -62,7 +54,7 @@
                 }
             @endphp
             <div
-                class="ml-4 w-full md:w-1/2 flex flex-col p-2 relative before:contents[''] before:absolute before:w-full before:h-20 before:bottom-0 before:left-0 z-50 before:-z-50 {{ $warna }} drop-shadow-lg before:rounded-2xl hover:before:translate-y-2 before:duration-300">
+                class="ml-4 w-full md:w-1/2 flex flex-col p-2 relative before:contents[''] before:absolute before:w-full before:h-20 before:bottom-0 before:left-0 z-50 before:-z-50 {{ $warna }} drop-shadow-lg before:rounded-2xl hover:before:translate-y-2 before:duration-300 mb-20 md:mb-0">
                 <div class="flex flex-col flex-1 p-2 py-4 md:px-4 bg-white rounded-xl shadow-lg h-auto relative">
                     <ul>
                         @foreach ($items as $programme)
@@ -73,7 +65,7 @@
             </div>
 
             <div
-                class="absolute w-2 h-2 bg-warna-01 top-1/2 -translate-y-1/2 rounded-full group-hover:scale-150 duration-300">
+                class="absolute w-2 h-2 bg-warna-01 top-1/2 left-0 -translate-x-1/2 md:left-1/2 -translate-y-1/2 rounded-full group-hover:scale-150 duration-300">
             </div>
         </div>
     @endforeach
