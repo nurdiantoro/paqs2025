37f70bbc8b34cfccb79961727e46c4fa496d065f
Nur Diantoro <nurdiantoro100@gmail.com>
2025-03-19 22:01:03 +0700

    1.56 Perubahan jadwal programme

--- resources/views/frontend/components/itinerary_paqs_meeting.blade.php
+++ resources/views/frontend/components/itinerary_paqs_meeting.blade.php
@@ -9,13 +9,27 @@
             'time_2' => '13.00',
         ],
         (object) [
-            'name' => 'Education and Research Committee Meeting',
+            'name' => 'Session 1 : Education Committee (Room 1)',
             'date' => '2025-08-23',
             'time_1' => '13.00',
             'time_2' => '15.00',
         ],
+
         (object) [
-            'name' => 'Sustainability and BIM Committee Meeting',
+            'name' => 'Session 2 : Digitalization Committee (Room 1)',
+            'date' => '2025-08-23',
+            'time_1' => '15.00',
+            'time_2' => '17.30',
+        ],
+        (object) [
+            'name' => 'Session 1 : Sustainability Committee (Room 2)',
+            'date' => '2025-08-23',
+            'time_1' => '13.00',
+            'time_2' => '15.00',
+        ],
+
+        (object) [
+            'name' => 'Session 2 : Research Committee (Room 2)',
             'date' => '2025-08-23',
             'time_1' => '15.00',
             'time_2' => '17.30',
@@ -24,7 +38,96 @@
 @endphp
 
 <div class="">
-    @foreach ($paqsMeetings as $paqsMeeting)
+    <div
+        class="relative w-full group flex flex-row justify-between items-center before:contents[''] before:w-[1px] before:h-full before:bg-slate-400 before:absolute before:top-0 before:left-1/2 before:-translate-x-1/2 after:contents[''] after:w-4 after:h-4 after:bg-sky-400  after:border-slate-400 after:shadow-md after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full">
+        <div class="w-[49%] px-3 rounded-lg mr-6 md:mr-12 text-right">
+            07.30 - 13.00
+        </div>
+        <div class="w-[49%] px-3 rounded-lg ml-6 md:ml-12">
+            Golf Tournament
+        </div>
+    </div>
+
+    {{-- divider --}}
+    <div
+        class="relative w-full group flex flex-row justify-between items-center before:contents[''] before:w-[1px] before:h-full before:bg-slate-400 before:absolute before:top-0 before:left-1/2 before:-translate-x-1/2 px-2 h-6">
+        <div class="w-[49%] px-3 rounded-lg mr-6 md:mr-12 text-right">
+
+        </div>
+        <div class="w-[49%] px-3 rounded-lg ml-6 md:ml-12">
+        </div>
+    </div>
+
+    {{-- Room 1 --}}
+    <div
+        class="relative w-full group flex flex-row justify-between items-center before:contents[''] before:w-[1px] before:h-full before:bg-slate-400 before:absolute before:top-0 before:left-1/2 before:-translate-x-1/2 px-2">
+        <div class="w-[49%] px-3 rounded-lg mr-6 md:mr-12 text-right">
+
+        </div>
+        <div class="w-[49%] px-3 rounded-lg ml-6 md:ml-12">
+            <strong>Room 1</strong>
+        </div>
+    </div>
+    <div
+        class="relative w-full group flex flex-row justify-between items-center before:contents[''] before:w-[1px] before:h-full before:bg-slate-400 before:absolute before:top-0 before:left-1/2 before:-translate-x-1/2 after:contents[''] after:w-4 after:h-4 after:bg-green-400  after:border-slate-400 after:shadow-md after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full px-2">
+        <div class="w-[49%] px-3 rounded-lg mr-6 md:mr-12 text-right">
+            13.00 - 15.00
+        </div>
+        <div class="w-[49%] px-3 rounded-lg ml-6 md:ml-12">
+            Session 1 : Education Committee
+        </div>
+    </div>
+    <div
+        class="relative w-full group flex flex-row justify-between items-center before:contents[''] before:w-[1px] before:h-full before:bg-slate-400 before:absolute before:top-0 before:left-1/2 before:-translate-x-1/2 after:contents[''] after:w-4 after:h-4 after:bg-green-400  after:border-slate-400 after:shadow-md after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full px-2">
+        <div class="w-[49%] px-3 rounded-lg mr-6 md:mr-12 text-right">
+            15.00 - 17.30
+        </div>
+        <div class="w-[49%] px-3 rounded-lg ml-6 md:ml-12">
+            Session 2 : Digitalization Committee
+        </div>
+    </div>
+
+    {{-- divider --}}
+    <div
+        class="relative w-full group flex flex-row justify-between items-center before:contents[''] before:w-[1px] before:h-full before:bg-slate-400 before:absolute before:top-0 before:left-1/2 before:-translate-x-1/2 px-2 h-6">
+        <div class="w-[49%] px-3 rounded-lg mr-6 md:mr-12 text-right">
+
+        </div>
+        <div class="w-[49%] px-3 rounded-lg ml-6 md:ml-12">
+        </div>
+    </div>
+
+    {{-- Room 2 --}}
+    <div
+        class="relative w-full group flex flex-row justify-between items-center before:contents[''] before:w-[1px] before:h-full before:bg-slate-400 before:absolute before:top-0 before:left-1/2 before:-translate-x-1/2 px-2">
+        <div class="w-[49%] px-3 rounded-lg mr-6 md:mr-12 text-right">
+
+        </div>
+        <div class="w-[49%] px-3 rounded-lg ml-6 md:ml-12">
+            <strong>Room 2</strong>
+        </div>
+    </div>
+    <div
+        class="relative w-full group flex flex-row justify-between items-center before:contents[''] before:w-[1px] before:h-full before:bg-slate-400 before:absolute before:top-0 before:left-1/2 before:-translate-x-1/2 after:contents[''] after:w-4 after:h-4 after:bg-red-400  after:border-slate-400 after:shadow-md after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full px-2">
+        <div class="w-[49%] px-3 rounded-lg mr-6 md:mr-12 text-right">
+            13.00 - 15.00
+        </div>
+        <div class="w-[49%] px-3 rounded-lg ml-6 md:ml-12">
+            Session 1 : Sustainability Committee
+        </div>
+    </div>
+    <div
+        class="relative w-full group flex flex-row justify-between items-center before:contents[''] before:w-[1px] before:h-full before:bg-slate-400 before:absolute before:top-0 before:left-1/2 before:-translate-x-1/2 after:contents[''] after:w-4 after:h-4 after:bg-red-400  after:border-slate-400 after:shadow-md after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full px-2">
+        <div class="w-[49%] px-3 rounded-lg mr-6 md:mr-12 text-right">
+            15.00 - 17.30
+        </div>
+        <div class="w-[49%] px-3 rounded-lg ml-6 md:ml-12">
+            Session 2 : Research Committee
+        </div>
+    </div>
+
+
+    {{-- @foreach ($paqsMeetings as $paqsMeeting)
         <div
             class="relative w-full group flex flex-row justify-between items-center before:contents[''] before:w-[1px] before:h-full before:bg-slate-400 before:absolute before:top-0 before:left-1/2 before:-translate-x-1/2 after:contents[''] after:w-4 after:h-4 after:bg-sky-400  after:border-slate-400 after:shadow-md after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:rounded-full py-2">
             <div class="w-[49%] group-hover:bg-slate-700 group-hover:text-white p-3 rounded-lg mr-6 md:mr-12 text-right">
@@ -38,5 +141,5 @@
             <div class="w-[49%] group-hover:bg-slate-700 group-hover:text-white p-3 rounded-lg ml-6 md:ml-12">
                 {{ $paqsMeeting->name }}</div>
         </div>
-    @endforeach
+    @endforeach --}}
 </div>
