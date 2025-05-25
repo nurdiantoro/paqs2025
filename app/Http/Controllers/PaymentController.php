0c9db7b996ace8ea4cfe4f1dadb9ca22018851d8
Nur Diantoro <nurdiantoro100@gmail.com>
2025-05-26 00:12:22 +0700

    4.1 Huge Update for espay (part1)
    1. response 4005402 ketika ada header yang kosong di inquiry
    2. PR untuk next nya, signature harus di simpan di database!

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -97,6 +97,7 @@ class PaymentController extends Controller
         // 2a. kalau gak ada, langsung kirim email tagihan yang nantinya redirect ke halaman invoice
         // ==================================================================
         if ($request->payment !== 'credit_card') {
+            session()->forget('order_data');
             return redirect(url('email/' . $no_invoice . '/' . $order->email));
         }
 
@@ -194,6 +195,30 @@ class PaymentController extends Controller
 
     public function inquiry(Request $request)
     {
+        // Cek apakah semua header ada dan tidak kosong
+        $requiredHeaders = ['Content-Type', 'X-TIMESTAMP', 'X-SIGNATURE', 'X-EXTERNAL-ID', 'X-PARTNER-ID', 'CHANNEL-ID',];
+        $missing = [];
+        foreach ($requiredHeaders as $header) {
+            if (empty($request->header($header))) {
+                $missing[] = $header;
+            }
+        }
+        if (!empty($missing)) {
+            return response()->json([
+                'responseCode' => '4005402',
+                'message' => 'Missing Mandatory Field {' . implode(', ', $missing) . '}',
+            ], 400);
+        }
+
+        // Header
+        $contentType   = $request->header('Content-Type');
+        $timestamp     = $request->header('X-TIMESTAMP');
+        $signature     = $request->header('X-SIGNATURE');
+        $externalId    = $request->header('X-EXTERNAL-ID');
+        $partnerId     = $request->header('X-PARTNER-ID');
+        $channelId     = $request->header('CHANNEL-ID');
+
+        // Request
         $partnerServiceId   = $request->input('partnerServiceId');
         $customerNo         = $request->input('customerNo');
         $virtualAccountNo   = $request->input('virtualAccountNo');
@@ -202,7 +227,14 @@ class PaymentController extends Controller
 
         if ($customerNo !== env('ESPAY_MERCHANT_CODE', 'SGWPTDMP')) {
             return response()->json([
-                'responseCode' => '4012400',
+                'responseCode' => '4015400',
+                'responseMessage' => 'Missing Mandatory Field',
+            ], 400);
+        }
+
+        if ($signature) {
+            return response()->json([
+                'responseCode' => '4015400',
                 'responseMessage' => 'Unauthorized Signature',
             ], 400);
         }
@@ -218,7 +250,7 @@ class PaymentController extends Controller
         }
 
         $response = [
-            'responseCode' => '2002400',
+            'responseCode' => '2005400',
             'responseMessage' => 'Success',
             'virtualAccountData' => [
                 'partnerServiceId' => $partnerServiceId,
@@ -240,76 +272,6 @@ class PaymentController extends Controller
                         ],
                     ],
                 ],
-                // 'additionalInfo' => [
-                //     'token' => '2023011167618274122',
-                //     'transactionDate' => now()->format('Y-m-d\TH:i:sP'),
-                //     'dataMerchant' => [
-                //         'kodeCa' => '880105',
-                //         'kodeSubCa' => '-',
-                //         'noKontrak' => '060223118342',
-                //         'namaPelanggan' => 'Yories Yolanda',
-                //         'angsuranKe' => '15',
-                //         'jmlBayarExcAdm' => 5000000.00,
-                //         'denda' => 0.00,
-                //         'feeCa' => 5000.00,
-                //         'feeSwitcher' => 0.00,
-                //         'totalAdmin' => 5000.00,
-                //         'jumlahBayar' => 5005000.00,
-                //         'minimumAmount' => 2000000.00,
-                //         'totalAngsuran' => 48,
-                //         'customer' => [
-                //             'email' => 'test@gmail.com',
-                //         ],
-                //         'jatuhTempo' => '2025-03-10',
-                //         'listBills' => [
-                //             [
-                //                 'billCode' => '01',
-                //                 'billName' => 'Bill Invoice 1',
-                //                 'billAmount' => [
-                //                     'value' => '70000.00',
-                //                     'currency' => 'IDR',
-                //                 ],
-                //                 'billSubCompany' => '00001',
-                //             ],
-                //             [
-                //                 'billCode' => '02',
-                //                 'billName' => 'Bill Invoice 2',
-                //                 'billAmount' => [
-                //                     'value' => '50000.00',
-                //                     'currency' => 'IDR',
-                //                 ],
-                //                 'billSubCompany' => '00002',
-                //             ],
-                //         ],
-                //         'shippingAddress' => [
-                //             'firstName' => 'Jokul',
-                //             'lastName' => 'Doe',
-                //             'address' => 'Jl. Teknologi Indonesia No. 25',
-                //             'city' => 'Jakarta',
-                //             'postalCode' => '12960',
-                //             'phoneNumber' => '6281828384858',
-                //             'countryCode' => 'IDN',
-                //         ],
-                //         'items' => [
-                //             [
-                //                 'id' => '1',
-                //                 'name' => 'Newlook Charm',
-                //                 'price' => '10000',
-                //                 'type' => 'Contact Lens',
-                //                 'url' => 'https://domain.com/products/detail?id=1',
-                //                 'quantity' => '1',
-                //             ],
-                //             [
-                //                 'id' => '2',
-                //                 'name' => 'Newlook Playful',
-                //                 'price' => '10000',
-                //                 'type' => 'Contact Lens',
-                //                 'url' => 'https://domain.com/products/detail?id=2',
-                //                 'quantity' => '1',
-                //             ],
-                //         ],
-                //     ],
-                // ],
             ],
         ];
 
