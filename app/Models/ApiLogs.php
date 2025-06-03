ff99d8fbf5ecc05266e179e89159a523591bd3ee
Nur Diantoro <nurdiantoro100@gmail.com>
2025-06-03 16:43:30 +0700

    5.7 Tambah table ApiLogs

--- app/Models/ApiLogs.php
+++ app/Models/ApiLogs.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ApiLogs extends Model
 {
     protected $guarded = [];
+    protected $casts = [
+        'headers' => 'array',
+        'body' => 'array',
+        'response' => 'array',
+    ];
 }
