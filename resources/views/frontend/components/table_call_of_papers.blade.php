4616bf195aea19eae20b6699f002ca8d18263375
Nur Diantoro <nurdiantoro100@gmail.com>
2025-08-15 00:23:08 +0700

    5.28 Update data table call for paper

--- resources/views/frontend/components/table_call_of_papers.blade.php
+++ resources/views/frontend/components/table_call_of_papers.blade.php
@@ -1,169 +1,229 @@
 <div class="w-full overflow-x-auto">
-    <table class="w-full text-center">
-        <thead class=" ">
-            <tr class="font-bold  text-white">
-                <th class="sticky top-0 text-center bg-warna-01 w-40">Time</th>
-                <th class="sticky top-0 text-center bg-warna-01">Duration</th>
-                <th class="sticky top-0 text-center bg-warna-01">Activities</th>
-                <th class="sticky top-0 text-center bg-warna-01">Room 1</th>
-                <th class="sticky top-0 text-center bg-warna-01">Room 2</th>
-                <th class="sticky top-0 text-center bg-warna-01">Room 3</th>
-                <th class="sticky top-0 text-center bg-warna-01">Speakers/Participants</th>
+    <table class="table table-bordered table-hover table-condensed">
+        <thead>
+            <tr>
+                <th rowspan="2" colspan="2" class="text-center bg-warna-01 text-white">TIME</th>
+                <th class=" bg-warna-01 text-white">Room: Dian B</th>
+                <th class=" bg-warna-01 text-white">Room: Dian C</th>
             </tr>
-
+            <tr>
+                <th colspan="2" class="text-center bg-warna-01 text-white">ACTIVITY</th>
+            </tr>
         </thead>
-        <tbody class="">
-            <tr class="group hover:bg-neutral-100">
-                <td>07:15 - 08:15</td>
-                <td>60 min</td>
-                <td>Registration</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+        <tbody>
+
+            <tr>
+                <td>7:30</td>
+                <td>8:00</td>
+                <td>Registration &amp; Morning Coffee</td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td>08:15 - 08:30</td>
-                <td>15 min</td>
-                <td>Opening</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>All Participants</td>
+            <tr>
+                <td>8:00</td>
+                <td>8:05</td>
+                <td>Opening tune </td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td rowspan="3">08:30 - 08:40</td>
-                <td></td>
-                <td>Prayer</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>MC</td>
+            <tr>
+                <td>8:05</td>
+                <td>8:10</td>
+                <td>Opening by MC </td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td></td>
-                <td>National Anthem & PAQS/IQSI March</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>Religious Leader</td>
+            <tr>
+                <td>8:10</td>
+                <td>8:20</td>
+                <td>Flag Ceremony following by President of PAQS and Chief of PAQS Countries</td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td></td>
-                <td>Welcome Speech by Chairperson</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>All Participants</td>
+            <tr>
+                <td>8:20</td>
+                <td>8:25</td>
+                <td>National Anthem </td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td>08:40 - 08:50</td>
-                <td>10 min</td>
-                <td>Speech by Other Leaders</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>Chairperson</td>
+            <tr>
+                <td>8:25</td>
+                <td>8:30</td>
+                <td>Praying by MC </td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td>08:50 - 09:00</td>
-                <td>10 min</td>
-                <td>Symbolic Gift Presentation</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>Other Leaders</td>
+            <tr>
+                <td>8:30</td>
+                <td>8:33</td>
+                <td>Video Teaser PAQS 2025</td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td rowspan="2">09:00 - 09:30</td>
-                <td rowspan="2">30 min</td>
-                <td>Opening Ceremony (Traditional Dance)</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>Chairperson</td>
+            <tr>
+                <td>8:33</td>
+                <td>8:43</td>
+                <td>Entertaiment Performance by Kemenpar</td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td>Keynote Speech 1 (Prof. Dr. (H.C.) Ir. Mohammed Ali Berawi, M.Eng. Sc, Ph.D.)</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>Performers</td>
+            <tr>
+                <td>8:43</td>
+                <td>8:53</td>
+                <td>Speech from Chairman of IQSI</td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td>09:30 - 09:45</td>
-                <td>15 min</td>
-                <td>Keynote 1 Discussion Session</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>Prof. Dr. (H.C.) Ir. Mohammed Ali Berawi, M.Eng. Sc, Ph.D.</td>
+            <tr>
+                <td>8:53</td>
+                <td>9:03</td>
+                <td>Speech from President of PAQS</td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td>09:45 - 10:15</td>
-                <td>30 min</td>
-                <td>Keynote Speech 2 (Prof. Sr. Dr. Eugene Seah)</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>Keynote Speaker 1 & Participants</td>
+            <tr>
+                <td>9:03</td>
+                <td>9:13</td>
+                <td>Speech from Pemprov</td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td>10:15 - 10:30</td>
-                <td>15 min</td>
-                <td>Keynote 2 Discussion Session</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>Prof. Sr. Dr. Eugene Seah</td>
+            <tr>
+                <td>9:13</td>
+                <td>9:23</td>
+                <td>Speech from the Ministry of Public Works of the Republic of Indonesia</td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td>10:30 - 10:45</td>
-                <td>15 min</td>
-                <td>Parallel Session Briefing</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+            <tr>
+                <td>9:23</td>
+                <td>9:33</td>
+                <td>Opening Ceremony </td>
+                <td> </td>
             </tr>
-            <tr class="group bg-neutral-300 text-warna-01">
-                <td class="bg-green-600 text-white">10:45 - 11:00</td>
-                <td class="bg-green-600 text-white">15 min</td>
-                <td class="bg-green-600 text-white" colspan="4">Coffee Break</td>
-                <td class="bg-green-600 text-white"></td>
+            <tr>
+                <td>9:33</td>
+                <td>9:43</td>
+                <td>Photo Session</td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td>11:00 - 12:00</td>
-                <td>60 min</td>
-                <td>Parallel Session (5 participants)</td>
-                <td>Parallel Session (5 participants)</td>
-                <td>Parallel Session (5 participants)</td>
-                <td>Parallel Session (5 participants)</td>
-                <td>Participants</td>
+            <tr>
+                <td>9:43</td>
+                <td>9:48</td>
+                <td>Closing by MC </td>
+                <td> </td>
             </tr>
-            <tr class="group bg-neutral-300 text-warna-01">
-                <td class="bg-orange-400 text-white">12:00 - 13:00</td>
-                <td class="bg-orange-400 text-white">60 min</td>
-                <td class="bg-orange-400 text-white" colspan="4">Lunch & Prayer Break (ISHOMA)</td>
-                <td class="bg-orange-400 text-white"></td>
+            <tr>
+                <td>9:48</td>
+                <td>10:30</td>
+                <td>Coffee Break &amp; Visit Booth</td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td>13:00 - 15:00</td>
-                <td>120 min</td>
-                <td>Parallel Session (10 participants)</td>
-                <td>Parallel Session (10 participants)</td>
-                <td>Parallel Session (10 participants)</td>
-                <td>Parallel Session (10 participants)</td>
-                <td>Participants</td>
+            <tr>
+                <td> </td>
+                <td> </td>
+                <td>Doorstop (Tempo ,Antara, Media Konstruksi)</td>
+                <td> </td>
             </tr>
-            <tr class="group hover:bg-neutral-100">
-                <td>15:00 - 17:00</td>
-                <td>120 min</td>
-                <td>Parallel Session (10 participants)</td>
-                <td>Parallel Session (10 participants)</td>
-                <td>Parallel Session (10 participants)</td>
-                <td>Parallel Session (10 participants)</td>
-                <td>Participants</td>
+            <tr>
+                <td>10:30</td>
+                <td>11:30</td>
+                <td>Keynote Speaker 1 : A/Prof. Dr. Sr. Eugene Seah &quot; Demystifying AI - What’s hype, what’s real,
+                    and why does it matter to us (in the construction industry) ?&quot;</td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>11:30</td>
+                <td>12:00</td>
+                <td>Keynote Sponsor: Sr. Dr. Nazirul Fariq &quot;Ushering in a Data-Driven Paradigm for Professional
+                    Practice through Industrial AI&quot;</td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>12:00</td>
+                <td>13:00</td>
+                <td>ISHOMA</td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>13:00</td>
+                <td>14:00</td>
+                <td>Keynote Speaker 2: Prof. D.Sc. Ir. Mohammed Ali Berawi, M.Eng.Sc. Ph.D. &quot; Developing Smart
+                    Sustainable City: Where Sustainability Meets Digital Innovation&quot;</td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td class="bg-orange-400 text-white">14:00</td>
+                <td class="bg-orange-400 text-white">14:10</td>
+                <td colspan="2" class="text-center bg-orange-400 text-white">Mobilisasi Peserta</td>
+            </tr>
+            <tr>
+                <td colspan="4" class="text-center bg-green-400 text-white">Parallel session</td>
+            </tr>
+            <tr>
+                <td>14:10</td>
+                <td>14:30</td>
+                <td>Invited Speaker Prof Mei-yung LEUNG</td>
+                <td>Invited Speaker Sri Lanka</td>
+            </tr>
+            <tr>
+                <td>14:30</td>
+                <td>14:45</td>
+                <td>AI-DCI<br />ID 11 - Stanley Hon Wah CHANG<br />”AI-Driven Construction Innovations: Leveraging
+                    artificial intelligence for predictive analytics, automation, and enhanced decision-making”</td>
+                <td>AI-DCI<br />ID 16 - Wing Yan Jacqueline LEUNG <br />”A Comparative Analysis of Digitalisation on
+                    Contract Management &amp; Administration in Hong Kong and Singapore”</td>
+            </tr>
+            <tr>
+                <td>14:45</td>
+                <td>15:00</td>
+                <td>AI-DCI<br />ID 03 - <br />”How Technology Facilitates Workdone Assessment During Determination”</td>
+                <td>AI-DCI<br />ID 19 - Diyana Syafiqah Binti Abd Razak <br />”Mitigating failures in construction
+                    Project: A conceptual framework for an innovative failure assessment Model (FAM)”</td>
+            </tr>
+            <tr>
+                <td class="bg-orange-400 text-white">15:00</td>
+                <td class="bg-orange-400 text-white">15:15</td>
+                <td colspan="2" class="text-center bg-orange-400 text-white">Coffee Break</td>
+            </tr>
+            <tr>
+                <td rowspan="3">15:15</td>
+                <td rowspan="3">16:00</td>
+                <td>AI-DCI<br />ID 05 - <br />”TerraLogic: AI-Driven Failure Analysis Using Computer Vision in Coal Mine
+                    Project PT Cipta Kridatama”</td>
+                <td>AI-DCI<br />ID 15 - Eric Yuen Chun Ting<br />”From Digital Platform Features to AI Integration:
+                    Transforming Quantity Surveying Practice and Contract Management in New Engineering Contract (NEC) ”
+                </td>
+            </tr>
+            <tr>
+                <td>AI-DCI<br />ID 06 - Tse Tung <br />”Harnessing Artificial Intelligence for Enhanced Quantity
+                    Surveying”</td>
+                <td>AI-DCI<br />ID 27 - Suranga Jayasena <br />&quot;AI in Construction: Academic vs Industry
+                    Discourse&quot;</td>
+            </tr>
+            <tr>
+                <td>AI-DCI<br />ID 07 - Zafira Nadia Maaz <br />”Explainable Artificial Intelligence (XAI) Model for
+                    Transparent and Trustworthy Construction Projects Tender Evaluation”</td>
+                <td>MPC<br />ID 18 - Kang Li<br />&quot;A Cost–Benefit Analysis of ImportingComplete Prefabricated Steel
+                    StructureBuildings from China to New Zealand&quot;</td>
+            </tr>
+            <tr>
+                <td>16:00</td>
+                <td>16:15</td>
+                <td>AI-DCI<br />ID 12 - Chen Guanming <br />”Research on Quantity Surveyor Management Combining BIM and
+                    Artificial Intelligence”</td>
+                <td>MPC<br />ID 25 - <br />&quot;Steel Modular Buildings in a Developing Context: A Lean Foundation for
+                    Smart and Sustainable Construction in Sri Lanka&quot;</td>
+            </tr>
+            <tr>
+                <td>16:15</td>
+                <td>16:30</td>
+                <td>AI-DCI<br />ID 13 - Arif Rahman<br />”Enhancing Real-Time Project Control through AI-Integrated WBS,
+                    CBS, and BIM in a Digital Twin Environment”</td>
+                <td>PNZD<br />ID 14 - Khairani Ayu Rizqi <br />&quot;Quantifying the Ecological Impact of Façade
+                    Retrofits: A Case Study of Mall Rejuvenation in South Jakarta&quot;</td>
+            </tr>
+            <tr>
+                <td>16:30</td>
+                <td>16:45</td>
+                <td>AI-DCI<br />Prof Mei-yung LEUNG<br />”Toward an AI-Enabled Value Management Process in Construction:
+                    A Technology-Focused Initial Evaluation&quot;</td>
+                <td>PNZD<br />ID 17 - Ruzanna Abd Rahman <br />&quot;A Comparative Regional Analysis for Green Building
+                    Rating Certification on<br />Data Centres&quot;</td>
+            </tr>
+            <tr>
+                <td class="bg-warna-01 text-white">16:45</td>
+                <td class="bg-warna-01 text-white">17:00</td>
+                <td colspan="2" class="text-center bg-warna-01 text-white">Penutup</td>
             </tr>
         </tbody>
     </table>
